7d6c9017cedfedd6d4113b561df5da68c33f5a41
Gustavo Tucci <gutucci232@gmail.com>
2026-06-11 15:29:58 -0300

    resolver conflito do merge

--- multi_agent_synapse/orquestrador_concierge.py
+++
@@ -1,8 +0,0 @@
-from google.adk.agents.llm_agent import Agent
-
-agent_concierge = Agent(
-    model='gemini-2.5-flash',
-    name='agent_concierge',
-    description='A helpful assistant for user questions.',
-    instruction='Answer user questions to the best of your knowledge',
-)
