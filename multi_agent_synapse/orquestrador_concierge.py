4a130806325a7f360cfe4ff8b32351e8e6dc7dc0
Gustavo Tucci <gutucci232@gmail.com>
2026-06-11 12:07:46 -0300

    feat:concierge

--- multi_agent_synapse/orquestrador_concierge.py
+++ multi_agent_synapse/orquestrador_concierge.py
@@ -2,7 +2,7 @@ from google.adk.agents.llm_agent import Agent
 
 agent_concierge = Agent(
     model='gemini-2.5-flash',
-    name='root_agent',
+    name='agent_concierge',
     description='A helpful assistant for user questions.',
     instruction='Answer user questions to the best of your knowledge',
 )
